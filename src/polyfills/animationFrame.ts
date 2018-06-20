4eab384b791db712739d527a45330de02c1ba8a5
严贤良 <yanxianliang@codyy.com>
2018-06-20 17:31:03 +0800

    fix

--- src/polyfills/animationFrame.ts
+++ src/polyfills/animationFrame.ts
@@ -4,5 +4,5 @@
  * @time：2018/6/6 19:12
  */
 
-window.requestAnimationFrame = (function () {return window.requestAnimationFrame || window.webkitRequestAnimationFrame ||(window as any).mozRequestAnimationFrame ||function (callback:any){window.setTimeout(callback, 6000 / 60);};})();
+window.requestAnimationFrame = (function () {return window.requestAnimationFrame || window.webkitRequestAnimationFrame ||(window as any).mozRequestAnimationFrame ||function (callback:any){window.setTimeout(callback, 1000 / 60);};})();
 window.cancelAnimationFrame=(function () {return window.cancelAnimationFrame || window.webkitCancelAnimationFrame||(window as any).mozCancelAnimationFrame ||function (handler:number){window.clearTimeout(handler);};})();
